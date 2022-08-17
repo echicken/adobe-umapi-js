d0f7f1f0c850743f585b17cf297bba545a97ebe6
Derek Mullin (Domain Admin) <dmullin_da@ocadu-ubuntu16.ocad.ca>
2022-08-17 12:16:21 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # adobe-umapi-js
 Interact with the Adobe User Management API from node.js
+
+## Example
+
+```js
+const fs = require('fs').promises;
+const UMAPI = require('adobe-umapi-js');
+
+(async () => {
+    const privateKey = await fs.readFile('/path/to/your/private.key', 'utf8');
+    const config = {
+        clientID: 'Your client ID',
+        clientSecret: 'Your client secret',
+        techAccount: 'Your technical account ID',
+        orgID: 'Your organization ID',
+        privateKey,
+    };
+    const umapi = new UMAPI.Client(config);
+    try {
+        const ui = await umapi.getUserInformation('some_user@some_domain');
+        console.log(ui);
+    } catch (err) {
+        console.error(err);
+    }
+})();
+```
+
+## Notes
+
+See the Client constructor in `index.js` for other options that can be specified in your Client config object if you wish to override the defaults.
+
+This is a bare minimum implementation to meet my present needs. Future developments would include:
+- Request throttling and backoff
+- Per-endpoint request limits
+- A Pool object with multiple clients for better throughput (there are per-client request limits and per-application limits)
+- Helper methods for other API endpoints (user action, user group action, get groups & profiles, etc.)
---
+++ index.js
@@ -0,0 +1,114 @@
+const axios = require('axios');
+const JWT = require('jsonwebtoken');
+
+class Client {
+
+    #clientID;
+    #clientSecret;
+    #orgID;
+    #techAccount;
+    #privateKey;
+    #accessToken;
+    #apiHost;
+    #apiEndpoint;
+    #imsHost;
+    #jwtEndpoint;
+    #scopes;
+
+    constructor({
+        clientID,
+        clientSecret,
+        orgID,
+        techAccount,
+        privateKey,
+        apiHost = 'usermanagement.adobe.io',
+        apiEndpoint = '/v2/usermanagement',
+        imsHost = 'ims-na1.adobelogin.com',
+        jwtEndpoint = '/ims/exchange/jwt',
+        scopes = ['ent_user_sdk'],
+    }) {
+        this.#clientID = clientID;
+        this.#clientSecret = clientSecret;
+        this.#orgID = orgID;
+        this.#techAccount = techAccount;
+        this.#privateKey = privateKey;
+        this.#apiHost = apiHost;
+        this.#apiEndpoint = apiEndpoint;
+        this.#imsHost = imsHost;
+        this.#jwtEndpoint = jwtEndpoint;
+        this.#scopes = scopes;
+    }
+
+    getJWT() {
+
+        const exp = ((new Date()).getTime() / 1000) + (60 * 60 * 24);
+        const payload = {
+            exp,
+            iss: this.#orgID,
+            sub: this.#techAccount,
+            aud: `https://${this.#imsHost}/c/${this.#clientID}`,
+        };
+        for (const scope of this.#scopes) {
+            payload[`https://${this.#imsHost}/s/${scope}`] = true;
+        }
+        const jwt = JWT.sign(payload, this.#privateKey, { algorithm: 'RS256' });
+
+        const options = {
+            method: 'post',
+            url: `https://${this.#imsHost}${this.#jwtEndpoint}`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cache-Control': 'no-cache',
+            },
+            data: new URLSearchParams({
+                client_id: this.#clientID,
+                client_secret: this.#clientSecret,
+                jwt_token: jwt,
+            }),
+        };
+        return axios(options);
+
+    }
+
+    reset() {
+        this.#accessToken = null;
+    }
+
+    async authorize(renew = false) {
+        const now = new Date();
+        if (!renew && this.#accessToken && this.#accessToken.expires.getTime() > now.getTime()) return;
+        const jwt = await this.getJWT();
+        this.#accessToken = {
+            token: jwt.data.access_token,
+            expires: now.setTime(now.getTime() + jwt.data.expires - (1000 * 60 * 60)), // Renew an hour early
+        };
+    }
+
+    async call(path, data) {
+        await this.authorize();
+        const options = {
+            method: data === undefined ? 'get' : 'post',
+            url: `https://${this.#apiHost}${this.#apiEndpoint}${path}`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'x-api-key': this.#clientID,
+                'Authorization': `Bearer ${this.#accessToken.token}`,
+            },
+        };
+        if (data !== undefined) options.data = data;
+        return axios(options);
+    }
+
+    async getUserInformation(userID) {
+        const res = await this.call(`/organizations/${this.#orgID}/users/${userID}`);
+        if (res.data === undefined) throw new Error('Invalid API response');
+        if (res.data.result !== 'success') return { err: new Error(`${res.data.result}: ${res.data.message}`) };
+        return res.data.user;
+    }
+
+}
+
+module.exports = {
+    Client,
+};
